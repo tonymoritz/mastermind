6a3b7ec48d395b07a9727d5c3d0a467fe99b0aef
Tony Moritz <tony@tonymoritz.com>
2021-06-29 13:25:31 -0500

    Add a bunch of .mm and code guess logic.

--- mastermind.js
+++ mastermind.js
@@ -1,53 +1,62 @@
-const colors = [
-  'black',
-  'green',
-  'blue',
-  'purple',
-  'red',
-  'orange',
-  'yellow',
-  'white'
-];
-const numberOfSlots = 5;
+window.mm = {
+  colorCode: [],
+  colorPicker: document.getElementById('color-picker'),
+  colors: [
+    'black',
+    'blue',
+    'green',
+    'orange',
+    'purple',
+    'red',
+    'white',
+    'yellow'
+  ],
+  guessColors: [],
+  numberOfSlots: 5
+};
 
 function generateCode() {
   function colorMaker() {
     let random;
     let returnCode = [];
 
-    for (let i = 0; i < numberOfSlots; i++) {
-      random = colors[Math.floor(Math.random() * colors.length)];
+    for (let i = 0; i < mm.numberOfSlots; i++) {
+      random = mm.colors[Math.floor(Math.random() * mm.colors.length)];
       returnCode.push(random);
     }
     return returnCode;
   }
 
-  const colorCode = colorMaker();
-  console.log(colorCode);
+  mm.colorCode = colorMaker();
+  console.log(mm.colorCode);
   const colorDisplay = document.getElementById('display-colors');
   colorDisplay.innerHTML = '';
 
-  for (let i = 0; i < numberOfSlots; i++) {
+  for (let i = 0; i < mm.numberOfSlots; i++) {
     colorDisplay.innerHTML += `<div
-      style="background-color: ${colorCode[i]}"
+      style="background-color: ${mm.colorCode[i]}"
       class="mastermind-circles"
     ></div>`;
   }
 }
 
-function sayColor(colorToSay) {
-  console.log(colorToSay);
-}
+function addToGuess(color) {
+  mm.guessColors.push(color);
+  console.log(mm.guessColors);
 
-const colorPicker = document.getElementById('color-picker');
+  for (let i = 0; i < mm.guessColors.length; i++) {
+    let selectedPeg = document.querySelector(`#guess-${i + 1}`);
+    selectedPeg.innerHTML = `<div style="background-color: ${mm.guessColors[i]}" class="mastermind-circles"></div>`;
+  }
+}
 
 generateCode();
 
 function generatePicker() {
-  for (let i = 0; i < colors.length; i++) {
-    colorPicker.innerHTML += `<div
-      style="background-color: ${colors[i]}"
-      class="mastermind-circles" onclick="sayColor('${colors[i]}')"
+  for (let i = 0; i < mm.colors.length; i++) {
+    mm.colorPicker.innerHTML += `<div
+      style="background-color: ${mm.colors[i]}"
+      class="mastermind-circles" onclick="addToGuess('${mm.colors[i]}')"
     ></div>`;
   }
 }
